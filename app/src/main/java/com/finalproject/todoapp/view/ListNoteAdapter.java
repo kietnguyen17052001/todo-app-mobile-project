d195dd49e1c4f289b783d3dd3902760e26fcef3f
nhat <nhat@gmail.com>
2022-06-12 00:44:12 +0700

    add task list layout and adapter

---
+++ app/src/main/java/com/finalproject/todoapp/view/ListNoteAdapter.java
@@ -0,0 +1,74 @@
+package com.finalproject.todoapp.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.finalproject.todoapp.R;
+import com.finalproject.todoapp.model.NewList;
+
+import java.util.ArrayList;
+
+public class ListNoteAdapter extends RecyclerView.Adapter<ListNoteAdapter.ViewHolder> {
+
+    private ArrayList<NewList> setNewListNote;
+    private OnCardViewListener onCardViewListener;
+
+    public ListNoteAdapter(ArrayList<NewList> setNewListNote, OnCardViewListener context) {
+        this.setNewListNote = setNewListNote;
+        this.onCardViewListener = context;
+    }
+
+    public void setData(ArrayList<NewList> lists) {
+        this.setNewListNote = lists;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.task_list_layout, parent, false);
+        return new ViewHolder(view, onCardViewListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.tvNewListName.setText(this.setNewListNote.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return setNewListNote.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView tvNewListName;
+    public OnCardViewListener onCardViewListener;
+
+        public ViewHolder(View view, OnCardViewListener onCardViewListener) {
+            super(view);
+            tvNewListName = view.findViewById(R.id.tv_newlist_name);
+            this.onCardViewListener = onCardViewListener;
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int posClick =getAdapterPosition();
+                    onCardViewListener.onCardViewCLick(posClick);
+                }
+            });
+        }
+    }
+
+    public interface OnCardViewListener {
+        void onCardViewCLick(int pos);
+
+    }
+
+}
